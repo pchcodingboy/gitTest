91f05c2c8ecdfa436ee68060994ccacb8e341390
pch <1104160530@qq.com>
2022-11-20 20:19:13 +0800

    第六次提交在master上修改代码

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -5,7 +5,7 @@
  */
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("aaa");
+        System.out.println("aaa1");
         System.out.println("bbb");
         System.out.println("fff");
     }
