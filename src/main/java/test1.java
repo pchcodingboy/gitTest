92080c97f8b8a5f8473ac488eb0ff0c7c2a670ea
pch <1104160530@qq.com>
2022-11-20 20:36:57 +0800

    第八次提交在dev03上修改代码

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -6,7 +6,8 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("aaa2");
-        System.out.println("ccc");
+        System.out.println("bbb");
+        System.out.println("fff");
 
     }
 }
